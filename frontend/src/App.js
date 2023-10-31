f23ddb948f7e160a6365f04f6c5fe6d62272a129
Sakshi_Bhatia <sakshibhatia9098@gmail.com>
2023-11-01 05:15:55 +0530

    Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-// pages & components
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Define the function to fetch data from the backend
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://workouts-56vi.onrender.com'); // Replace with your Render backend URL
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    // Call the function to fetch data when the component mounts
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
@@ -12,10 +28,7 @@ function App() {
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route 
-              path="/" 
-              element={<Home />} 
-            />
+            <Route path="/" element={<Home data={data} />} /> {/* Pass the fetched data to the Home component */}
           </Routes>
         </div>
       </BrowserRouter>
